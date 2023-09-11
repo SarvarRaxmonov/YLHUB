1a4557d42c61bf9a904f13dc2e0d9d5e2084be75
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-09-11 18:19:15 +0500

    feat: apps created vebinar , library, student_test

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Created at"), blank=True, null=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_("Updated at"), blank=True, null=True
+    )
+
+    class Meta:
+        abstract = True
+
---
+++ apps/vebinar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VebinarConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "vebinar"
